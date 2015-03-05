73887a404e452efede990471f5de8fe94d927a77
lgosson <pudge_gosson21@hotmail.com>
2015-03-05 08:33:52 -0800

    lab7
    working

---
+++ FunkyHippo3.0-55af954e23affe24b41dcbc16a9511488e72b1a7/FunkyHippo/Controllers/AlbumsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FunkyHippo.DAL;
+using FunkyHippo.Models;
+
+namespace FunkyHippo.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        //private FunkyHippoContext db = new FunkyHippoContext();
+        private IAlbumRepository repo;
+        //called by mvc
+        public AlbumsController()
+        {
+            repo = new AlbumRepository(new FunkyHippoContext());
+        }
+
+        public AlbumsController(IAlbumRepository fakeRepo)
+        {
+            repo = fakeRepo;
+        }
+
+        // GET: Albums
+        public ActionResult Index()
+        {
+            return View(repo.GetAlbums());
+        }
+
+        // GET: Albums/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Album album = repo.GetAlbumByID((int)id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // GET: Albums/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AlbumID,Title,Artist,Genre,Release")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+
+                repo.InsertAlbum(album);
+                    repo.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(album);
+        }
+
+        // GET: Albums/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = repo.GetAlbumByID((int)id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AlbumID,Title,Artist,Genre,Release")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.UpdateAlbum(album);
+                repo.Save();
+                
+                return RedirectToAction("Index");
+            }
+            return View(album);
+        }
+
+        // GET: Albums/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = repo.GetAlbumByID((int)id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            
+            repo.DeleteAlbum(id);
+            repo.Save();
+            
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
